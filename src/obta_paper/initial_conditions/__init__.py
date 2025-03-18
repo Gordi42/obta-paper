3d7928bcde76c45c6f72ccaa898d12fcc728e46f
Gordi42 <silvano98@hotmail.de>
2025-03-18 18:21:48 +0100

    add the initial condition for the random phase state in the shallow water model

--- src/obta_paper/initial_conditions/__init__.py
+++ src/obta_paper/initial_conditions/__init__.py
@@ -9,6 +9,7 @@ from lazypimp import setup
 # ================================================================
 if TYPE_CHECKING:  # pragma: no cover
     from .shallow_water_jet import ShallowWaterJet
+    from .shallow_water_random import ShallowWaterRandom
 
 # ================================================================
 #  Setup lazy loading
@@ -21,6 +22,7 @@ base = "obta_paper.initial_conditions"
 
 all_imports_by_origin = {
     f"{base}.shallow_water_jet": ["ShallowWaterJet"],
+    f"{base}.shallow_water_random": ["ShallowWaterRandom"],
 }
 
 setup(__name__, all_modules_by_origin, all_imports_by_origin)
